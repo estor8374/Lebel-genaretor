9c7e7a858ed3cfd6624554ad3f910b87b53a8c8f
estor8374 <estore8374@gmail.com>
2026-02-04 14:30:47 +0530

    Update createOrder.js

--- netlify/functions/createOrder.js
+++ netlify/functions/createOrder.js
@@ -1,22 +1,47 @@
 const Razorpay = require("razorpay");
 
-exports.handler = async function () {
+exports.handler = async function (event) {
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY,
-  key_secret: process.env.RAZORPAY_SECRET
-});
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
 
-const options = {
-  amount: 1000, // ₹10 = 1000 paise
-  currency: "INR"
-};
+  try {
 
-const order = await razorpay.orders.create(options);
+    const razorpay = new Razorpay({
+      key_id: process.env.rzp_live_SBFRjqhpikBmjf,     // Set in Netlify ENV
+      key_secret: process.env.lmcFoqSifoBUfTiM9sqbHS6Y // Set in Netlify ENV
+    });
 
-return {
-  statusCode: 200,
-  body: JSON.stringify(order)
-};
+    const options = {
+      amount: 1000, // 10 INR = 1000 paise
+      currency: "INR",
+      receipt: "order_" + Date.now()
+    };
 
+    const order = await razorpay.orders.create(options);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(order)
+    };
+
+  } catch (err) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Order Failed",
+        message: err.message
+      })
+    };
+
+  }
 };
